1cab90ed7afaf3928503de9a7827affabb640649
saravanakumar <iamsarank@gmail.com>
2024-05-03 12:11:52 +0530

    complete file upload

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,26 +1,85 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
+
 
 export default function Profile() {
+  const dispatch = useDispatch();
+  const fileRef = useRef(null);
+
+  const [image, setImage] = useState(undefined);
+  const [imagePercent, setImagePercent] = useState(0);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({});
+  const [uploadSuccess, setUploadSuccess] = useState(false);
+
   const { currentUser } = useSelector((state) => state.user);
 
+  useEffect(() => {
+    if (image) {
+      handleFileUpload(image);
+    }
+  }, [image]);
 
+  const handleFileUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePercent(Math.round(progress));
+      },
+      (error) => {
+        setImageError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, profilePicture: downloadURL })
+        );
+      }
+    );
+  };
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
-  const handleDeleteAccount = async () => {}
 
-  const handleSignOut = async () => {}
+  const handleDeleteAccount = async () => { }
+
+  const handleSignOut = async () => { }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className='flex flex-col gap-4'>
+
+        <input type="file"
+          ref={fileRef} hidden accept="image/*"
+          onChange={(e) => setImage(e.target.files[0])} />
+
+        {/* 
+      firebase storage rules:  
+      allow read;
+      allow write: if
+      request.resource.size < 2 * 1024 * 1024 &&
+      request.resource.contentType.matches('image/.*') */}
+
+
         <img src={currentUser.profilePicture}
           alt='profile'
-          className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2' />
+          className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2'
+          onClick={() => fileRef.current.click()} />
         <input
           defaultValue={currentUser.username}
           type='text'
@@ -29,6 +88,19 @@ export default function Profile() {
           className='bg-slate-100 rounded-lg p-3'
           onChange={handleChange}
         />
+        <p className='text-sm self-center'>
+          {imageError ? (
+            <span className='text-red-700'>
+              Error uploading image (file size must be less than 2 MB)
+            </span>
+          ) : imagePercent > 0 && imagePercent < 100 ? (
+            <span className='text-slate-700'>{`Uploading: ${imagePercent} %`}</span>
+          ) : imagePercent === 100 ? (
+            <span className='text-green-700'>Image uploaded successfully</span>
+          ) : (
+            ''
+          )}
+        </p>
         <input
           defaultValue={currentUser.email}
           type='email'
@@ -58,7 +130,7 @@ export default function Profile() {
         <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
           Sign out
         </span>
-      </div>      
+      </div>
     </div>
 
   )
